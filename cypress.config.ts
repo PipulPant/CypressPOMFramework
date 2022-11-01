83b3413e2ceb5653bf6ff01a2d0be237d60baca3
PipulPant <pipulpant1@gmail.com>
2022-11-01 16:49:44 +0545

    updated config with mochawesome reporter :lock:

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'demoGuru',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
-    baseUrl: "https://demo.guru99.com/",
+    baseUrl: 'https://demo.guru99.com/',
   },
-});
+})
